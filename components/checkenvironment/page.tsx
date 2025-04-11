9d2b3e9b6164f0c2234b16b1b431ef58aa4c998d
Jerry <15170551+jerryguo1020@user.noreply.gitee.com>
2025-04-11 21:01:07 +0800

    修改了部分内容

--- components/checkenvironment/page.tsx
+++ components/checkenvironment/page.tsx
@@ -1,7 +1,5 @@
 import React from'react';
-import One from '@/components/checkenvironment/one';
 
-import Login from '@/components/logincheck/login';
 import Naybar from '@/components/laybar-we-can-see/laybar';
 export default function Home() {
     return (
