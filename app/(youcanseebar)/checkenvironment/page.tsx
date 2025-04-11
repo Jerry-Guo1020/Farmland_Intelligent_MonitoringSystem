9f7a0479483c3fdd474479bac953b2520e5cb5bb
Jerry <15170551+jerryguo1020@user.noreply.gitee.com>
2025-04-11 20:55:18 +0800

    修改了部分内容

--- app/(youcanseebar)/checkenvironment/page.tsx
+++ app/(youcanseebar)/checkenvironment/page.tsx
@@ -1,7 +1,6 @@
 import React from'react';
-import One from '@/components/checkenvironment/one';
 
-import Login from '@/components/logincheck/login';
+// import Login from '@/components/logincheck/login';
 import Naybar from '@/components/laybar-we-can-see/laybar';
 
 export default function CheckPage() {
